13319fece97fb4102fad6525967abf2a140e45ff
Richard Schwarcz <riso.schwarcz@gmail.com>
2022-10-27 08:25:25 +0200

    1.Zadanie almost done

--- modules/internalForces.py
+++ modules/internalForces.py
@@ -2,6 +2,47 @@ from dataclasses import dataclass, field
 from modules.model import Bar
 
 
+class BaseFunctions:
+    def loadPosition(self, barLength, load):
+        a_distance = barLength - barLength * load.F_position
+        b_distance = barLength - a_distance
+        return a_distance, b_distance
+
+    def bendingMoment_F(self, load, distance):
+        [a_distance, b_distance] = distance
+        barLength = a_distance + b_distance
+
+        Ma = -load.F * a_distance * b_distance**2 / barLength**2
+        Mb = load.F * a_distance**2 * b_distance / barLength**2
+        return Ma, Mb
+
+    def bendingMoment_Q(self, load, distance):
+        [a_distance, b_distance] = distance
+        barLength = a_distance + b_distance
+
+        Ma = -load.Q * barLength**2 / 12
+        Mb = load.Q * barLength**2 / 12
+        return Ma, Mb
+
+    def shearForce_F(self, load, distance):
+        [a_distance, b_distance] = distance
+        barLength = a_distance + b_distance
+
+        Va = -load.F * b_distance**2 / \
+            barLength**3 * (barLength + 2 * a_distance)
+        Vb = -load.F * a_distance**2 / \
+            barLength**3 * (barLength + 2 * b_distance)
+        return Va, Vb
+
+    def shearForce_Q(self, load, distance):
+        [a_distance, b_distance] = distance
+        barLength = a_distance + b_distance
+
+        Vb = -load.Q * barLength / 2
+        Va = -load.Q * barLength / 2
+        return Va, Vb
+
+
 @dataclass
 class Load:
     Q: int = 0
@@ -10,51 +51,26 @@ class Load:
 
 
 @dataclass
-class BendingMoment:
+class InternalForces_primary(BaseFunctions):
     load: Load
     bar: Bar = field(repr=False)
 
-    barLength: int = field(init=False)
     Ma: int = field(init=False, default_factory=lambda: 0)
     Mb: int = field(init=False, default_factory=lambda: 0)
 
-    def __post_init__(self) -> None:
-        self.barLength = self.bar.len
-
-        if self.load.F != 0:
-            a_distance = self.barLength - self.barLength * self.load.F_position
-            b_distance = self.barLength - a_distance
-
-            self.Ma = -self.load.F * a_distance * b_distance**2 / self.barLength**2
-            self.Mb = self.load.F * a_distance**2 * b_distance / self.barLength**2
-
-        if self.load.Q != 0:
-            self.Ma = -self.load.Q * self.barLength**2 / 12
-            self.Mb = self.load.Q * self.barLength**2 / 12
-
-
-@dataclass
-class ShearForce:
-    load: Load
-    bar: Bar = field(repr=False)
-
-    barLength: int = field(init=False)
     Va: int = field(init=False, default_factory=lambda: 0)
     Vb: int = field(init=False, default_factory=lambda: 0)
 
     def __post_init__(self) -> None:
-        self.barLength = self.bar.len
+        # define local variables
+        barLength = self.bar.len
+        load = self.load
+        distance = self.loadPosition(barLength, load)
 
+        # assign to self
         if self.load.F != 0:
-            a_distance = self.barLength - self.barLength * self.load.F_position
-            b_distance = self.barLength - a_distance
-
-            self.Va = -self.load.F * b_distance**2 / \
-                self.bar.len**3 * (self.bar.len + 2 * a_distance)
-
-            self.Vb = -self.load.F * a_distance**2 / \
-                self.bar.len**3 * (self.bar.len + 2 * b_distance)
-
+            [self.Ma, self.Mb] = self.bendingMoment_F(load, distance)
+            [self.Va, self.Vb] = self.shearForce_F(load, distance)
         if self.load.Q != 0:
-            self.Va = -self.load.Q * self.barLength / 2
-            self.Vb = -self.load.Q * self.barLength / 2
+            [self.Ma, self.Mb] = self.bendingMoment_Q(load, distance)
+            [self.Va, self.Vb] = self.shearForce_Q(load, distance)
--- modules/plot.py
+++ modules/plot.py
@@ -1,3 +1,16 @@
 def plotSchema(points):
     point_1 = [A.x, A.y]
     point_2 = [B.x, B.y]
+
+
+def highlightNotZero(x):
+    if -1*10**-3 < x < 1*10**-3:
+        # gray
+        color = "#f2f2f2"
+    elif x > 1*10**-3:
+        # green
+        color = "#E1FFD5"
+    elif x < 1*10**-3:
+        # red
+        color = "#FFDCD5"
+    return f"background: {color}"
